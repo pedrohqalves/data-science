364f7cda6edfb0ae45c5ff7560dfb414ccb3cd3b
Pedro Alves <quadrosalvespedro@gmail.com>
2019-12-22 14:13:47 -0300

    Added XGB Model
    Added XGB Model

--- kaggle/ashrae-energy-consumption/Ashrae_PedroAlves.py
+++ kaggle/ashrae-energy-consumption/Ashrae_PedroAlves.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[1]:
+# In[ ]:
 
 
 import numpy as np
@@ -10,20 +10,20 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-# In[2]:
+# In[ ]:
 
 
 pd.set_eng_float_format(accuracy=4)
 
 
-# In[3]:
+# In[ ]:
 
 
 train = pd.read_csv(r'E:\Users\quadr\Documents\datascience-arquivos\Kaggle\Ashrae_Energy_Prediction\train.csv')
 w_train = pd.read_csv(r'E:\Users\quadr\Documents\datascience-arquivos\Kaggle\Ashrae_Energy_Prediction\weather_train.csv')
 
 
-# In[4]:
+# In[ ]:
 
 
 b_meta = pd.read_csv(r'E:\Users\quadr\Documents\datascience-arquivos\Kaggle\Ashrae_Energy_Prediction\building_metadata.csv')
@@ -36,43 +36,45 @@ b_meta = pd.read_csv(r'E:\Users\quadr\Documents\datascience-arquivos\Kaggle\Ashr
 # ## 2 - Steam
 # ## 3 - Hotwater
 
-# In[5]:
+# In[ ]:
 
 
 train_total = pd.merge(train,b_meta,how='left', on= 'building_id')
 
 
-# In[6]:
+# In[ ]:
 
 
-train_total2 = pd.merge(train_total,w_train, on=['site_id','timestamp'],how='left')
+train_total = pd.merge(train_total,w_train, on=['site_id','timestamp'],how='left')
 
 
 # ### Now that we merged the dataframes, we will drop the timestamp column so it doesnt affect our model
 
-# In[7]:
-
-
-train_total2=train_total2.drop('timestamp', axis=1)
-
-
 # In[ ]:
 
 
-train_total2.head()
+train_total = train_total.drop('timestamp', axis=1)
 
 
 # In[ ]:
 
 
 # To check how much missing values we have
-train_total2.count().plot(kind='barh')
+train_total.count().plot(kind='barh')
+
+
+# In[ ]:
+
+
+# Deleting floor_count and year_built and cloud coverage that have a lot of missing values
+
+train_total = train_total.drop('floor_count', axis=1)
+train_total = train_total.drop('year_built', axis=1)
+train_total = train_total.drop('cloud_coverage', axis=1)
 
 
 # ### The variables that posess the least amount of entries are:
-# #### year_built 
-# #### floor_count
-# #### cloud_coverage
+# 
 # #### precip_depth
 # #### sea_level_pressure
 # #### wind_direction
@@ -89,14 +91,14 @@ train_total2.count().plot(kind='barh')
 
 
 # Seems like Education Buildings consume a lot of Energy, as Services too
-train_total2.groupby('primary_use').meter_reading.mean().plot(kind='barh')
+train_total.groupby('primary_use').meter_reading.mean().plot(kind='barh')
 
 
 # In[ ]:
 
 
 # Here we can see how site_ids are occupated
-train_total2.groupby('primary_use').site_id.mean().plot(kind='bar')
+train_total.groupby('primary_use').site_id.mean().plot(kind='bar')
 
 
 # In[ ]:
@@ -107,7 +109,7 @@ train_total2.groupby('primary_use').site_id.mean().plot(kind='bar')
 
 #As expected, site 7 and 11 have lower temperature readings, that would explain why it needs more energy consumption for example heating
 # To try to confirm that theory, lets see how energy is distributed in that site, compared to others.
-train_total2.groupby('site_id').air_temperature.mean().plot(kind='bar')
+train_total.groupby('site_id').air_temperature.mean().plot(kind='bar')
 
 
 # In[ ]:
@@ -115,196 +117,397 @@ train_total2.groupby('site_id').air_temperature.mean().plot(kind='bar')
 
 # As expected, site 7 and 11 have higher meter means, which means they depend more on other types of energy that aren't
 # electricity.
-train_total2.groupby('site_id').meter.mean().plot(kind='barh')
-
-
-# In[ ]:
-
-
-# Seems like Education, Lodging and Technology tend to have higher buildings, 
-# that would explain a bit why Education consumes a lot of energy
-train_total2.groupby('primary_use').floor_count.mean().plot(kind='bar')
+train_total.groupby('site_id').meter.mean().plot(kind='barh')
 
 
 # In[ ]:
 
 
 # The higher the bar, means the occupation has different energy consumption types
-train_total2.groupby('primary_use').meter.mean().plot(kind='bar')
+train_total.groupby('primary_use').meter.mean().plot(kind='bar')
 
 
 # In[ ]:
 
 
 # Parking and services provide the biggest size in square feet
-train_total2.groupby('primary_use').square_feet.mean().plot(kind='bar')
+train_total.groupby('primary_use').square_feet.mean().plot(kind='bar')
 
 
 # In[ ]:
 
 
 # Education is by far the most common type of building in this dataset
-train_total2.groupby('primary_use').building_id.count().plot(kind='bar')
+train_total.groupby('primary_use').building_id.count().plot(kind='bar')
 
 
 # In[ ]:
 
 
 # Same here with sites that are very differently distributed when we look at how many buildings each of them have
-train_total2.groupby('site_id').building_id.count().plot(kind='barh')
+train_total.groupby('site_id').building_id.count().plot(kind='barh')
 
 
 # ### We are gonna fill the empty values in the dataframe to go to the next step, feature engineering
 
-# In[8]:
+# In[ ]:
 
 
-train_total2.year_built = train_total2.year_built.fillna(train_total2.year_built.mean())
-train_total2.cloud_coverage = train_total2.cloud_coverage.fillna(train_total2.cloud_coverage.mean())
-train_total2.precip_depth_1_hr = train_total2.precip_depth_1_hr.fillna(0)
-train_total2.sea_level_pressure = train_total2.sea_level_pressure.fillna(train_total2.sea_level_pressure.mean())
-train_total2.wind_direction = train_total2.wind_direction.fillna(train_total2.wind_direction.mean())
-train_total2.wind_speed = train_total2.wind_speed.fillna(0)
-train_total2.dew_temperature = train_total2.dew_temperature.fillna(train_total2.dew_temperature.mean())
-train_total2.air_temperature = train_total2.air_temperature.fillna(train_total2.air_temperature.mean())
+train_total.precip_depth_1_hr = train_total.precip_depth_1_hr.fillna(0)
+train_total.sea_level_pressure = train_total.sea_level_pressure.fillna(train_total.sea_level_pressure.mean())
+train_total.wind_direction = train_total.wind_direction.fillna(train_total.wind_direction.mean())
+train_total.wind_speed = train_total.wind_speed.fillna(0)
+train_total.dew_temperature = train_total.dew_temperature.fillna(train_total.dew_temperature.mean())
+train_total.air_temperature = train_total.air_temperature.fillna(train_total.air_temperature.mean())
 
 
 # In[ ]:
 
 
-train_total2.floor_count.unique()
-
-
-# #### With floor_count, we can see that there's no building with 0 floors, so we can assume nan means a building that has only base level.
-# 
-# #### To help modeling we will substitute nan with 0 for floor_count
-
-# In[9]:
-
-
-train_total2.floor_count = train_total2.floor_count.fillna(0)
+train_total.count().plot(kind='barh',figsize=(15,5))
 
 
 # In[ ]:
-
-
-train_total2.count().plot(kind='barh',figsize=(15,5))
-
-
-# In[10]:
 
 
 # Before that we need to get dummies for all the categorical data
-train_total2 = pd.get_dummies(train_total2)
+train_total = pd.get_dummies(train_total)
 
 
-# In[11]:
+# In[ ]:
 
 
 from sklearn.feature_selection import SelectPercentile, f_regression, f_classif
 
 
-# In[12]:
+# In[ ]:
 
 
-x = train_total2.drop('meter_reading', axis=1)
+x = train_total.drop('meter_reading', axis=1)
 
 
-# In[13]:
+# In[ ]:
 
 
-y = train_total2.meter_reading
+y = train_total.meter_reading
 
 
-# In[14]:
+# In[ ]:
 
 
 percentile = SelectPercentile(f_regression,percentile=30)
 
 
-# In[15]:
+# In[ ]:
 
 
 x_eng = percentile.fit_transform(x,y)
 
 
-# In[16]:
+# In[ ]:
 
 
 cols_selected = percentile.get_support(indices=True)
 
 
-# In[17]:
+# In[ ]:
 
 
 x_eng = x.iloc[:,cols_selected]
 
 
+# In[ ]:
+
+
+x_eng.columns
+
+
 # ## We're going to start with a simple linear regression to see how our accuracy goes
 
-# In[21]:
+# In[ ]:
 
 
 from sklearn.model_selection import train_test_split
 
 
-# In[23]:
+# In[ ]:
 
 
 x_train, x_test, y_train, y_test = train_test_split(x_eng,y,test_size = 0.3, random_state=42)
 
 
-# In[19]:
+# In[ ]:
 
 
 from sklearn import linear_model
 
 
-# In[25]:
+# In[ ]:
 
 
 lr = linear_model.LinearRegression()
 
 
-# In[26]:
+# In[ ]:
 
 
 lr.fit(x_train,y_train)
 
 
-# In[27]:
+# In[ ]:
 
 
-y_predic = lr.predict(x_test)
+y_lr = lr.predict(x_test)
 
 
-# In[33]:
+# In[ ]:
 
 
 from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 
-# In[34]:
+# In[ ]:
 
 
-msre = np.sqrt(mean_squared_error(y_test,y_predic))
-mae = mean_absolute_error(y_test,y_predic)
+msre = np.sqrt(mean_squared_error(y_test,y_lr))
+mae = mean_absolute_error(y_test,y_lr)
 
 
-# In[35]:
+# In[ ]:
 
 
 print(msre)
 print(mae)
 
 
-# In[39]:
+# In[ ]:
 
 
 lr.score(x_test,y_test)
 
 
-# ## Doesnt seem like the linear regression was a good estimate
+# In[ ]:
+
+
+get_ipython().system('pip install xgboost')
+
+
+# ## Trying a XGBoost Model
+
+# In[ ]:
+
+
+import xgboost as xgb
+
+
+# In[ ]:
+
+
+modelxgb = xgb.XGBRegressor(n_estimators=10, learning_rate=0.08, gamma=0, subsample=0.75,
+                           colsample_bytree=1, max_depth=3)
+
+
+# In[ ]:
+
+
+modelxgb.fit(x_train,y_train)
+
+
+# In[ ]:
+
+
+y_xgb = modelxgb.predict(x_test)
+
+
+# In[ ]:
+
+
+msre_xgb = np.sqrt(mean_squared_error(y_test,y_xgb))
+mae_xgb = mean_absolute_error(y_test,y_xgb)
+print(msre_xgb)
+print(mae_xgb)
+
+
+# In[ ]:
+
+
+import pickle
+
+
+# In[ ]:
+
+
+pickle.dump(lr, open('lr.pickle', 'wb'))
+
+
+# In[ ]:
+
+
+pickle.dump(modelxgb, open('modelxgb.pickle', 'wb'))
+
+
+# # Going for the test set
+
+# In[1]:
+
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pickle
+
+
+# In[2]:
+
+
+b_meta = pd.read_csv(r'E:\Users\quadr\Documents\datascience-arquivos\Kaggle\Ashrae_Energy_Prediction\building_metadata.csv')
+
+
+# ### Mantaining only the columns the model will use
+# #### 'building_id', 'meter', 'site_id', 'square_feet', 'sea_level_pressure', 'wind_speed', 'primary_use_Education', 'primary_use_Office'
+
+# In[3]:
+
+
+w_test = pd.read_csv(r'E:\Users\quadr\Documents\datascience-arquivos\Kaggle\Ashrae_Energy_Prediction\weather_test.csv')
+
+
+# In[4]:
+
+
+w_test.columns
+
+
+# In[5]:
+
+
+w_test = w_test.drop ('air_temperature', axis = 1)
+w_test = w_test.drop ('cloud_coverage', axis = 1)
+w_test = w_test.drop ('dew_temperature', axis = 1)
+w_test = w_test.drop ('precip_depth_1_hr', axis = 1)
+w_test = w_test.drop ('wind_direction', axis = 1)
+
+
+# In[6]:
+
+
+w_test.head()
+
+
+# In[7]:
+
+
+b_meta.columns
+
+
+# In[8]:
+
+
+b_meta_test = b_meta[['site_id', 'building_id', 'primary_use', 'square_feet']]
+
+
+# In[9]:
+
+
+b_meta_test = pd.get_dummies(b_meta_test)
+
+
+# In[10]:
+
+
+b_meta_test.columns
+
+
+# ### Mantaining only the columns the model will use
+# #### 'building_id', 'meter', 'site_id', 'square_feet', 'sea_level_pressure', 'wind_speed', 'primary_use_Education', 'primary_use_Office'
+
+# In[11]:
+
+
+b_meta_test = b_meta_test[['site_id','building_id','square_feet','primary_use_Education', 'primary_use_Office']]
+
+
+# In[12]:
+
+
+modelxgb = pickle.load(open('modelxgb.pickle','rb'))
+
+
+# In[13]:
+
+
+test = pd.read_csv(r'E:\Users\quadr\Documents\datascience-arquivos\Kaggle\Ashrae_Energy_Prediction\test.csv')
+
+
+# In[14]:
+
+
+test.count()
+
+
+# In[15]:
+
+
+test = pd.merge(test,b_meta_test,how = 'left', on = 'building_id')
+test = pd.merge(test, w_test, how = 'left', on = ['site_id','timestamp'])
+test = test.drop('timestamp', axis = 1)
+
+
+# In[16]:
+
+
+test.wind_speed = test.wind_speed.fillna(test.wind_speed.mean())
+test.sea_level_pressure = test.sea_level_pressure.fillna(test.sea_level_pressure.mean())
+
+
+# In[17]:
+
+
+test.head()
+
+
+# In[18]:
+
+
+test2 = test[['row_id','building_id', 'meter', 'site_id', 'square_feet', 'sea_level_pressure', 'wind_speed', 'primary_use_Education', 'primary_use_Office']]
+
+
+# In[19]:
+
+
+test2.head()
+
+
+# In[20]:
+
+
+test2['meter_reading'] = modelxgb.predict(test2.iloc[:,1:])
+
+
+# In[22]:
+
+
+predictions = test2[['row_id','meter_reading']]
+
+
+# In[23]:
+
+
+predictions = predictions.set_index('row_id')
+
+
+# In[24]:
+
+
+predictions.to_csv('submission_pedroalvesxgb.csv')
+
+
+# In[25]:
+
+
+predictions.shape
+
 
 # In[ ]:
 
